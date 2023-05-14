9c44c27ee150d304711527e2033b6d044cd14334
Janne Cools <janne.cools@ugent.be>
2023-05-14 11:32:43 +0200

    mst wordt getoond met pie charts

--- src/buildMST.py
+++ src/buildMST.py
@@ -38,34 +38,49 @@ class MST_Builder():
         nodes = self.xdim*self.ydim
         print(nodes)
         graph = nx.Graph()
-        #graph.add_nodes_from([i for i in range(nodes)])
+
         som = np.reshape(som, (nodes, channels))
         #print(som)
         print(som.shape)
         for x in range(nodes):
+            # graph.add_node()
             for y in range(x+1, nodes):
                 difference = abs(som[x]-som[y])
                 weight = np.product([i for i in difference if not math.isnan(i)])
                 graph.add_edge(x, y, weight=weight)
         tree = nx.minimum_spanning_tree(graph)
 
-        a = np.array(som)
-
         pos = graphviz_layout(tree, prog="neato")
         nx.draw_networkx_edges(tree, pos)
-        # for node in tree.nodes:
-        #     node_weights = [i if not math.isnan(i) and i > 0 else 0 for i in som[node]]
-        #     plt.subplot(pos[node][0], pos[node][1], aspect=1)
-        #     a = plt.pie(
-        #         node_weights,
-        #         center=pos[node],
-        #         radius=0.1
-        #     )
-        # tree.nodes[0] = plt.pie([i if not math.isnan(i) and i > 0 else 0 for i in som[0]])
-        nx.draw_networkx_nodes(tree, pos, node_size=100)
+        # nx.draw_networkx_nodes(tree, pos, node_size=100)
 
         ax = plt.gca()
-        ax.margins(0.2)
+        ax.margins(0.01)
+
+        for node in tree.nodes:
+            node_weight = [i if not math.isnan(i) and i > 0 else 0 for i in som[node]]
+            node_pos = pos[node]
+            self.draw_nodes(node_weight, node_pos[0], node_pos[1], ax)
+
         plt.axis("off")
         #plt.savefig("twopi_mst_edges")
         plt.show()
+
+    # https://stackoverflow.com/questions/56337732/how-to-plot-scatter-pie-chart-using-matplotlib
+    @staticmethod
+    def draw_nodes(data, xpos, ypos, ax):
+        # for incremental pie slices
+        cumsum = np.cumsum(data)
+        cumsum = cumsum / cumsum[-1]
+        pie = [0] + cumsum.tolist()
+
+        for r1, r2 in zip(pie[:-1], pie[1:]):
+            angles = np.linspace(2 * np.pi * r1, 2 * np.pi * r2)
+            x = [0] + np.cos(angles).tolist()
+            y = [0] + np.sin(angles).tolist()
+
+            xy = np.column_stack([x, y])
+
+            ax.scatter([xpos], [ypos], marker=xy, s=100)
+
+        return ax
