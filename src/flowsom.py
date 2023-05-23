85dc6c68aaced4e5e72402d71bd5cf6aafa54c61
Janne Cools <janne.cools@ugent.be>
2023-05-23 18:37:01 +0200

    juiste kolommen worden verwijderd uit de anndata

--- src/flowsom.py
+++ src/flowsom.py
@@ -2,7 +2,7 @@ import math
 import random
 
 import networkx as nx
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, ClassifierMixin
 import numpy as np
 import pandas
 import readfcs
@@ -22,7 +22,6 @@ class FlowSom(BaseEstimator):
         colsToUse=None,
         n_clusters=10,
         maxMeta=None,
-        importance=None,
         seed=None,
         xdim=10,
         ydim=10
@@ -30,113 +29,130 @@ class FlowSom(BaseEstimator):
         self.input = input
         self.pattern = pattern
         self.silent = silent
-        self.colsToUse = colsToUse
-        self.n_clusters = n_clusters
         self.maxMeta = maxMeta
         self.seed = seed
         self.xdim = xdim
         self.ydim = ydim
+        self.cols = len(colsToUse)
         self.som = None
         self.np_data = None
         if seed is not None:
             random.seed(seed)
         if isinstance(input, str):
             self.adata = readfcs.read(self.input)
-            self.remove_unused_data()
-        self.build_som()
-        self.build_mst(networkx=True)
-        self.cluster(networkx=True)
+            self.remove_unused_data(colsToUse)
+        self.build_som(xdim, ydim, len(colsToUse))
+        self.build_mst(xdim, ydim, networkx=True)
+        self.cluster(n_clusters, xdim, ydim, networkx=True)
 
-    def remove_unused_data(self):
-        cols = self.adata.var_names
-        indices = [i for i, col in enumerate(cols) if col not in self.colsToUse]
-        indices = np.flip(np.sort(indices))
+    def remove_unused_data(self, columns):
+        cols = self.adata.uns["meta"]["channels"]["$PnN"]
+        indices = cols.index.astype(np.intc)
+        unused = [indices[i] for i, col in enumerate(cols) if col not in columns]
+        unused = np.flip(np.sort(unused))
         data = self.adata.X
-        for index in indices:
-            data = np.delete(data, index, axis=1)
+        for index in unused:
+            data = np.delete(data, index-1, axis=1)
         self.np_data = np.nan_to_num(data)
-        # self.np_data = data
+        # sla de data op van de kolommen die gebruikt moeten worden
+        self.adata.uns["used_data"] = self.np_data
 
-    def build_som(self):
+    def build_som(self, xdim, ydim, cols):
         # bepaal radius
-        grid = [(x, y) for x in range(self.xdim) for y in range(self.ydim)]
+        grid = [(x, y) for x in range(xdim) for y in range(ydim)]
         nhbrdist = squareform(pdist(grid, metric="chebyshev"))
         radius = np.quantile(nhbrdist, 0.67)
 
         # self.som = MiniSom(
-        #     x=self.xdim, y=self.ydim, input_len=self.np_data.shape[1],
+        #     x=xdim, y=ydim, input_len=self.np_data.shape[1],
         #     sigma=radius, learning_rate=0.05, random_seed=self.seed
         # )
         self.som = MiniSom(
-            x=self.xdim, y=self.ydim, input_len=len(self.colsToUse),
-            learning_rate=0.05
+            x=xdim, y=ydim, input_len=cols, learning_rate=0.05, random_seed=self.seed
         )
         self.som.train(self.np_data, 100, verbose=True)
-        self.som_weights = np.reshape(self.som.get_weights(), (self.xdim*self.ydim, len(self.colsToUse)))
+        self.adata.uns["som_weights"] = np.reshape(self.som.get_weights(), (xdim*ydim, cols))
         # win = self.som.win_map(self.np_data)
         # print(win)
 
         # update anndata
-        self.adata.uns["som_weights"] = self.som_weights
+        self.adata.uns["som_weights"] = self.adata.uns["som_weights"]
 
-        plot_SOM(self.som.get_weights(), self.xdim, self.ydim)
+        plot_SOM(self.som.get_weights(), xdim, ydim)
 
-    def build_mst(self, networkx=True):
+    def build_mst(self, xdim, ydim, networkx=True):
         if networkx:
-            self.__build_mst_networkx()
+            self.__build_mst_networkx(xdim, ydim)
         else:
-            self.__build_mst_igraph()
+            self.__build_mst_igraph(xdim, ydim)
 
-    def __build_mst_networkx(self, clusters=None):
-        nodes = self.xdim * self.ydim
+    def __build_mst_networkx(self, xdim, ydim, clusters=None):
+        nodes = xdim * ydim
         print(nodes)
         graph = nx.Graph()
 
         # print(som)
-        print(self.som_weights.shape)
+        weights = self.adata.uns["som_weights"]
+        print(weights.shape)
         for x in range(nodes):
             # graph.add_node()
             for y in range(x + 1, nodes):
-                difference = abs(self.som_weights[x] - self.som_weights[y])
+                difference = abs(weights[x] - weights[y])
                 weight = np.sum(
                          [i for i in difference if not math.isnan(i)])
                 graph.add_edge(x, y, weight=weight)
         tree = nx.minimum_spanning_tree(graph)
-        plot_MST_networkx(tree, self.som_weights, clusters)
+        if clusters is not None:
+            self.adata.uns["mst_clustering"] = tree
+        else:
+            self.adata.uns["mst_som"] = tree
+        plot_MST_networkx(tree, weights, clusters)
 
-    def __build_mst_igraph(self, clusters=None):
-        dim = self.xdim * self.ydim
+    def __build_mst_igraph(self, xdim, ydim, clusters=None):
+        dim = xdim * ydim
         graph = ig.Graph(n=dim)
         weights = []
 
+        som_weights = self.adata.uns["som_weights"]
         for x in range(dim):
             for y in range(x + 1, dim):
-                difference = abs(self.som_weights[x] - self.som_weights[y])
+                difference = abs(som_weights[x] - som_weights[y])
                 weight = np.sum(
                     [i for i in difference if not math.isnan(i)])
                 graph.add_edges([(x, y)])
                 weights.append(weight)
         graph.es["weight"] = weights
         tree = graph.spanning_tree(weights=graph.es["weight"], return_tree=True)
-        plot_MST_igraph(tree, self.som_weights, clusters)
+        if clusters is not None:
+            self.adata.uns["mst_clustering"] = tree
+        else:
+            self.adata.uns["mst_som"] = tree
+        plot_MST_igraph(tree, som_weights, clusters)
 
-    def cluster(self, networkx=True):
-        clustering = AgglomerativeClustering(n_clusters=self.n_clusters, linkage="average")
-        som_weights = np.nan_to_num(self.som_weights)
+    def cluster(self, n_clusters, xdim, ydim, networkx=True):
+        clustering = AgglomerativeClustering(n_clusters=n_clusters, linkage="average")
+        som_weights = np.nan_to_num(self.adata.uns["som_weights"])
         clustering.fit(som_weights)
         print(clustering.labels_)
         self.adata.uns["cluster_labels"] = clustering.labels_
         if networkx:
-            self.__build_mst_networkx(clustering.labels_)
+            self.__build_mst_networkx(xdim, ydim, clustering.labels_)
         else:
-            self.__build_mst_igraph(clustering.labels_)
+            self.__build_mst_igraph(xdim, ydim, clustering.labels_)
 
 
     def set_params(self, **params):
         self.__dict__.update(params)
 
     def fit(self, X: pandas.DataFrame, y=None):
-        raise NotImplementedError
+        x = ClassifierMixin()
+        # x.
+        # self.build_som(self.xdim, self.ydim, self.cols)
+        # print(X)
+        # estimator = BaseEstimator()
+        # estimator.fit(X.values, self.som_weights)
+        return self
+        # raise NotImplementedError
 
     def predict(self, X: pandas.DataFrame):
         raise NotImplementedError
@@ -149,13 +165,22 @@ class FlowSom(BaseEstimator):
         :param lazy: dask if lazy else pandas
         :return: DataFrame van de ingegeven data
         """
-        raise NotImplementedError
+        # raise NotImplementedError
+        return self.adata.to_df()
 
     def as_adata(self, lazy=True):
         """
         :param lazy: dask if lazy else pandas
         :return: AnnData met in .uns de clustering in een FlowSom
         """
+        return self.adata
+
+    def report(self, filename: str):
+        report = open("../verslag.pdf", "r")
+        lines = report.readlines()
+
+        output = open(filename, "w")
+        output.writelines(lines)
 
 
 if __name__ == "__main__":
@@ -164,8 +189,17 @@ if __name__ == "__main__":
                        "PE-A", "PE-Cy5-A", "PE-Cy7-A"]
     cols_levine_13 = ["CD45", "CD45RA", "CD19", "CD11b", "CD4", "CD8", "CD34",
                       "CD20", "CD33", "CD123", "CD38", "CD90", "CD3"]
+    cols_levine_32 = ["CD45RA", "CD133", "CD19", "CD22", "CD11b", "CD4", "CD8",
+                      "CD34", "Flt3", "CD20", "CXCR4", "CD235ab", "CD45",
+                      "CD123", "CD321", "CD14", "CD33", "CD47", "CD11c", "CD7",
+                      "CD15", "CD16", "CD44", "CD38", "CD13", "CD3", "CD61",
+                      "CD117", "CD49d", "HLA-DR", "CD64", "CD41"]
     flowsom = FlowSom(
-        input="../Gelabelde_datasets/Levine_13dim.fcs",
-        colsToUse=cols_levine_13,
+        input="../Gelabelde_datasets/FlowCAP_ND.fcs",
+        colsToUse=cols_flowcap_nd,
         seed=10
     )
+    # adata = flowsom.as_adata()
+    # used_data = adata.uns["used_data"]
+    # flowsom.fit(pandas.DataFrame(used_data))
+    # flowsom.report("test.txt")
